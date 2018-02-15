9acfce5c7cfeba6f8035b8f2d8acd354433dc617
Dylan Rothfeld <drothfel@gmail.com>
2018-02-15 11:37:41 -0800

    elderface interface class and field values

---
+++ Elderface/Classes/Elderface.swift
@@ -0,0 +1,41 @@
+//
+//  Elderface.swift
+//  Elderface
+//
+//  Created by Dylan Rothfeld on 2/15/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+class Elderface {
+    // Fields
+    var user: String
+    var audioSpeed: Int
+    var menuTextSize: Int
+    var menuTextColor: UIColor
+    var menuColor: UIColor
+    var menuBorderColor: UIColor
+    var menuButtonColor: UIColor
+    var menuButtonOutlineColor: UIColor
+    var menuPosition: Int
+    var screenBackgroundColor: UIColor
+    var screenTextSize: Int
+    
+    // Constructor
+    init(user: String) {
+        self.user = user
+        self.audioSpeed = audio_speed_normal
+        self.menuTextSize = menu_text_normal
+        self.menuPosition = menu_top
+        self.screenTextSize = screen_text_normal
+        self.menuTextColor = UIColor.black
+        self.menuColor = UIColor.white
+        self.menuBorderColor = UIColor.black
+        self.menuButtonColor = UIColor.white
+        self.menuButtonOutlineColor = UIColor.black
+        self.screenBackgroundColor = UIColor.white
+    }
+}
+
+
---
+++ Elderface/Constants/ElderfaceFieldValues.swift
@@ -0,0 +1,32 @@
+//
+//  ElderfaceFieldValues.swift
+//  Elderface
+//
+//  Created by Dylan Rothfeld on 2/15/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import Foundation
+
+// Menu Position
+let menu_top = 1
+let menu_bottom = 2
+let menu_left = 3
+let menu_right = 4
+
+// Audio Speed
+let audio_speed_very_slow = -2
+let audio_speed_slow = -1
+let audio_speed_normal = 0
+let audio_speed_fast = 1
+let audio_speed_very_fast = 2
+
+// Menu Text Size
+let menu_text_small = 16
+let menu_text_normal = 45
+let menu_text_large = 112
+
+// Screen Text Size
+let screen_text_small = 24
+let screen_text_normal = 56
+let screen_text_large = 125
